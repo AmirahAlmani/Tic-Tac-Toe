4d839a206da7cb7abf30c7fc8bc8a3bcd02ac60d
Amirah Almani <amirahalmani@gmail.com>
2020-02-05 13:19:58 +0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-console.log("it's work ?");
+console.log("it's work ?"); 
 
 
-let game = document.querySelectorAll(".game");
+let game = document.querySelectorAll(".game"); //To call game class in html file
 
+let soundClick = new Audio('clickB.mp3'); // To add sound whin user clike
+let soundWin = new Audio('win.mp3'); // To add sound when X or O win
+let soundLose=new Audio('lose.mp3'); //To add sound when lose or tie
 
-
-let count = 0;
+let count = 0; //
 
 function clickEvent() {
 
-    // let target = event.target;
+    
     if (count % 2 === 0) {
+        soundClick.play();
         console.log("click x");
         this.classList += " x";
         this.removeEventListener('click', clickEvent)
 
 
     } else {
+        soundClick.play();
         console.log("click o");
         this.classList += " o";
         this.removeEventListener('click', clickEvent)
@@ -53,14 +57,14 @@ resetButton.addEventListener('click', reset)
 
 
 let arr = [
-    ["r1c1", 'r1c2', 'r1c3'],
-    ['r2c1', 'r2c2', 'r2c3'],
-    ['r3c1', 'r3c2', 'r3c3'],
-    ['r1c1', 'r2c1', 'r3c1'],
-    ['r1c2', 'r2c2', 'r3c2'],
-    ['r1c3', 'r2c3', 'r3c3'],
-    ['r1c1', 'r2c2', 'r3c3'],
-    ['r1c3', 'r2c2', 'r3c1']
+    ["#r1c1", '#r1c2', '#r1c3'],
+    ['#r2c1', '#r2c2', '#r2c3'],
+    ['#r3c1', '#r3c2', '#r3c3'],
+    ['#r1c1', '#r2c1', '#r3c1'],
+    ['#r1c2', '#r2c2', '#r3c2'],
+    ['#r1c3', '#r2c3', '#r3c3'],
+    ['#r1c1', '#r2c2', '#r3c3'],
+    ['#r1c3', '#r2c2', '#r3c1']
 ];
 
 
@@ -79,13 +83,15 @@ let winning = function () {
 
     for (let x = 0; x < arr.length; x++) {
 
-        const string1 = "#" + arr[x][0];
-        const string2 = "#" + arr[x][1];
-        const string3 = "#" + arr[x][2];
+        const string1 =  arr[x][0];
+        const string2 =  arr[x][1];
+        const string3 =  arr[x][2];
         if (document.querySelector(string1).classList.value.includes('x') &&
             document.querySelector(string2).classList.value.includes('x') &&
             document.querySelector(string3).classList.value.includes('x')) {
             console.log(string1 + " " + string2 + " " + string3)
+
+            soundWin.play();
             window.alert("X Win")
             reset();
         } else
@@ -93,11 +99,14 @@ let winning = function () {
             document.querySelector(string2).classList.value.includes('o') &&
             document.querySelector(string3).classList.value.includes('o')) {
             console.log(string1 + " " + string2 + " " + string3)
+
+            soundWin.play();
             window.alert("O Win")
             reset();
         } 
     }
     if (count == 9) {
+        soundLose.play();
         window.alert("Tie")
         reset();
     }
